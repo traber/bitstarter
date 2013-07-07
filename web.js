9e70cce278f5c5fe818ac9da864605c3c661c8d3
Thomas Traber <thomas.traber@gmx.de>
2013-07-07 15:31:45 +0200

    Changed web.js to read in the index.html file.

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var text = fs.readFileSync("index.html").toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
